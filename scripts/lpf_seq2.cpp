f9e238e156945fac6bcfa2c1dd843ac43858f558
Ahmad S. Salama <vadrif.draco@gmail.com>
2023-05-11 02:55:49 +0300

    More bugfixes and refactoring to LPF script

--- scripts/lpf_seq2.cpp
+++ scripts/lpf_seq2.cpp
@@ -1,16 +1,29 @@
 #include <chrono>
+#include <math.h>
 #include <fstream>
 #include <iostream>
 #include <string.h>
 
-void apply_mask(long long (&offsets)[9], float(&mask)[9], long long(&range)[2], char* (&processed), char* (&original)) {
+void apply_mask(long long(&offsets)[9], float(&mask)[9], long long(&range)[2], char* (&processed), char* (&original)) {
 
     for (long long i = range[0]; i < range[1]; ++i) {
-        processed[i] = 
-            float(
-                original[i + offsets[0]] * mask[0] + original[i + offsets[1]] * mask[1] + original[i + offsets[2]] * mask[2] +
-                original[i + offsets[3]] * mask[3] + original[i + offsets[4]] * mask[4] + original[i + offsets[5]] * mask[5] +
-                original[i + offsets[6]] * mask[6] + original[i + offsets[7]] * mask[7] + original[i + offsets[8]] * mask[8]);
+        processed[i] = round(float(
+            original[i + offsets[0]] * mask[0] + original[i + offsets[1]] * mask[1] + original[i + offsets[2]] * mask[2] +
+            original[i + offsets[3]] * mask[3] + original[i + offsets[4]] * mask[4] + original[i + offsets[5]] * mask[5] +
+            original[i + offsets[6]] * mask[6] + original[i + offsets[7]] * mask[7] + original[i + offsets[8]] * mask[8]));
+    }
+
+}
+
+void apply_maskv(long long(&offsets)[9], float(&mask)[9], long long(&range)[2], long long(&rowsize), char* (&processed), char* (&original)) {
+
+    for (long long i = range[0]; i < range[1]; i += rowsize) {
+        for (long long k = i; k < i + 3; k++) {
+            processed[k] = round(float(
+                original[k + offsets[0]] * mask[0] + original[k + offsets[1]] * mask[1] + original[k + offsets[2]] * mask[2] +
+                original[k + offsets[3]] * mask[3] + original[k + offsets[4]] * mask[4] + original[k + offsets[5]] * mask[5] +
+                original[k + offsets[6]] * mask[6] + original[k + offsets[7]] * mask[7] + original[k + offsets[8]] * mask[8]));
+        }
     }
 
 }
@@ -32,6 +45,22 @@ int main(int argc, const char** argv) {
     process_input.read(data, data_size);
     process_input.close();
 
+    // Preprocessing: Filter mask(s)
+    // float mask[9] = {
+
+    //     0.0625f, 0.1250f, 0.0625f,
+    //     0.1250f, 0.2500f, 0.1250f,
+    //     0.0625f, 0.1250f, 0.0625f,
+
+    // }; // Gaussian smoothing
+    float mask[9] = {
+
+        1 / 9.0f, 1 / 9.0f, 1 / 9.0f,
+        1 / 9.0f, 1 / 9.0f, 1 / 9.0f,
+        1 / 9.0f, 1 / 9.0f, 1 / 9.0f,
+
+    }; // Normal smoothing
+
     // Preprocessing: Convenience naming
     long long w = data_width * 3; // data width in bytes
     long long& h = data_height; // data height in pixels
@@ -46,37 +75,18 @@ int main(int argc, const char** argv) {
     // Preprocessing: Edge pixels start/end bytes (inclusive, exclusive)
     long long topedge[] = { 3, w - 3 };
     long long bottedge[] = { s - w + 3, s - 3 };
-    long long leftedge[] = { w, s - w };
-    long long rightedge[] = { w + w - 3, s - w - 3 };
+    long long leftedge[] = { w, s - w }; // Dealt with in pixel triples rather than bytes
+    long long rightedge[] = { w + w - 3, s - 3 }; // Dealt with in pixel triples rather than bytes
 
-    // For the aid of visualization...
-    // 012 345 678 901 (w = 12, s = 36)
-    // xxx xxx xxx xxx
-    // xxx xxx xxx xxx
-    // xxx xxx xxx xxx
+    // Preprocessing: For the aid of visualization...
+    //    012 345 678 901
+    // 00 xxx xxx xxx xxx 11
+    // 12 xxx xxx xxx xxx 23
+    // 24 xxx xxx xxx xxx 35
+    // 36 xxx xxx xxx xxx 47
+    // 48 xxx xxx xxx xxx 59 (s = 60)
 
-    // Preprocessing: Iterators
-    long long i, k;
-
-    // Preprocessing: Filter mask
-    // 1/16    1/8    1/16
-    // 1/8     1/4    1/8
-    // 1/16    1/8    1/16
-    // float mask[9] = { 16.0f, 8.0f, 16.0f, 8.0f, 4.0f, 8.0f, 16.0f, 8.0f, 16.0f }; // Gaussian smoothing
-    // float mask[9] = { 9.0f, 9.0f, 9.0f, 9.0f, 9.0f, 9.0f, 9.0f, 9.0f, 9.0f }; // Normal smoothing
-    float mask[9] = { 
-        
-        0.0625f, 0.1250f, 0.0625f,
-        0.1250f, 0.2500f, 0.1250f,
-        0.0625f, 0.1250f, 0.0625f,
-        
-    }; // Gausianisni smoothing
-
-    // Process it
-    char* blurred_data = (char*) malloc(data_size);
-    auto start = std::chrono::steady_clock::now();
-
-    // Regarding offsets:
+    // Preprocessing: Regarding offsets:
     // "- w" takes us to the pixel (and channel) directly above
     // "+ 3" takes us to the pixel (and channel) to the right
     // "+ w" takes us to the pixel (and channel) directly below
@@ -84,108 +94,98 @@ int main(int argc, const char** argv) {
 
     long long topleftoffsets[9] = {
 
-        0,    0,    0,
-        0,    0,    3,
-        0,    w,    w+3
-        
+        0,      0,      0,
+        0,      0,      3,
+        0,      w,      w + 3
+
     };
-    apply_mask(topleftoffsets, mask, topleft, blurred_data, data);
 
     long long toprightoffsets[9] = {
-        
-        0,    0,    0,
-        -3,   0,    0,
-        w-3,  w,    0
-        
+
+        0,      0,      0,
+        -3,     0,      0,
+        w - 3,  w,      0
+
     };
+
+    long long bottleftoffsets[9] = {
+
+        0,      -w,     -w + 3,
+        0,      0,      3,
+        0,      0,      0
+
+    };
+
+    long long bottrightoffsets[9] = {
+
+        -w - 3, -w,     0,
+        -3,     0,      0,
+        0,      0,      0
+
+    };
+
+    long long topedgeoffsets[9] = {
+
+        0,      0,      0,
+        -3,     0,      3,
+        w - 3,  w,      w + 3
+
+    };
+
+    long long bottedgeoffsets[9] = {
+
+        -w - 3, -w,     -w + 3,
+        -3,     0,      3,
+        0,      0,      0,
+
+    };
+
+    long long leftedgeoffsets[9] = {
+
+        0,      -w,     -w + 3,
+        0,      0,      3,
+        0,      w,      w + 3,
+
+    };
+
+    long long rightedgeoffsets[9] = {
+
+        -w - 3, -w,     0,
+        -3,     0,      0,
+        w - 3,  w,      0,
+
+    };
+
+    long long alloffsets[9] = {
+
+        -w - 3, -w,     -w + 3,
+        -3,     0,      3,
+        w - 3,  w,      w + 3,
+
+    };
+
+    // Process it
+    char* blurred_data = (char*) malloc(data_size);
+    auto start = std::chrono::steady_clock::now();
+    // Processing: Corners
+    apply_mask(topleftoffsets, mask, topleft, blurred_data, data);
     apply_mask(toprightoffsets, mask, topright, blurred_data, data);
-
-    long long bottleftoffsets[9] = { 
-        
-        0,    -w,   -w+3,
-        0,    0,    3,
-        0,    0,    0
-
-    };
     apply_mask(bottleftoffsets, mask, bottleft, blurred_data, data);
-
-    long long bottrightoffsets[9] = { 
-        
-        -w-3, -w,   0,
-        -3,   0,    0,
-        0,    0,    0
-
-    };
     apply_mask(bottrightoffsets, mask, bottright, blurred_data, data);
-
-    long long topedgeoffsets[9] = { 
-        
-        0,    0,    0,
-        -3,   0,    3,
-        w-3,  w,    w+3
-    
-    };
+    // Processing: Edges
     apply_mask(topedgeoffsets, mask, topedge, blurred_data, data);
-
-    long long bottedgeoffsets[9] = { 
-        
-        -w-3, -w,   -w+3,
-        -3,   0,    3,
-        0,    0,    0,
-
-    };
     apply_mask(bottedgeoffsets, mask, bottedge, blurred_data, data);
-
-    // TODO: make it use apply_mask
-    long long leftedgeoffsets[9] = { 
-        
-        0,   -w,   -w+3,
-        0,   0,    3,
-        0,   w,    w+3,
-
-    };
-    for (i = leftedge[0]; i < leftedge[1]; i += w) {
-        for (k = i; k < i + 3; k++) {
-            blurred_data[k] =
-                data[k + leftedgeoffsets[0]] * mask[0] + data[k + leftedgeoffsets[1]] * mask[1] + data[k + leftedgeoffsets[2]] * mask[2] +
-                data[k + leftedgeoffsets[3]] * mask[3] + data[k + leftedgeoffsets[4]] * mask[4] + data[k + leftedgeoffsets[5]] * mask[5] +
-                data[k + leftedgeoffsets[6]] * mask[6] + data[k + leftedgeoffsets[7]] * mask[7] + data[k + leftedgeoffsets[8]] * mask[8];
-        }
-    }
-
-    // TODO: make it use apply_mask
-    long long rightedgeoffsets[9] = { 
-        
-        -w-3, -w,   0,
-        -3,   0,    0,
-        w-3,  w,    0,
-
-    };
-    for (i = rightedge[0]; i < rightedge[1]; i += w) {
-        for (k = i; k < i + 3; k++) {
-            blurred_data[k] =
-                data[k + rightedgeoffsets[0]] * mask[0] + data[k + rightedgeoffsets[1]] * mask[1] + data[k + rightedgeoffsets[2]] * mask[2] +
-                data[k + rightedgeoffsets[3]] * mask[3] + data[k + rightedgeoffsets[4]] * mask[4] + data[k + rightedgeoffsets[5]] * mask[5] +
-                data[k + rightedgeoffsets[6]] * mask[6] + data[k + rightedgeoffsets[7]] * mask[7] + data[k + rightedgeoffsets[8]] * mask[8];
-        }
-    }
-
-    long long offsets[9] = { 
-        
-        -w-3, -w,   -w+3,
-        -3,   0,    3,
-        w-3,  w,    w+3,
-
-    };
+    apply_maskv(leftedgeoffsets, mask, leftedge, w, blurred_data, data);
+    apply_maskv(rightedgeoffsets, mask, rightedge, w, blurred_data, data);
+    // Processing: Body
     long long iteration_range[2];
-    for (i = w + 3; i < s - w; i += w) {
+    for (long long i = w + 3; i < s - w; i += w) {
 
         iteration_range[0] = i;
         iteration_range[1] = i + w - 3 - 3;
-        apply_mask(offsets, mask, iteration_range, blurred_data, data);
+        apply_mask(alloffsets, mask, iteration_range, blurred_data, data);
 
     }
-
     auto end = std::chrono::steady_clock::now();
     long long t = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 
